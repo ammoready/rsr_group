05215c6288239da6bd5d965cc2d9a0f84c199a32
Justin Wilson <justin@jwils.co>
2016-11-17 13:35:13 -0500

    Fix tests

--- spec/rsr_group/base_spec.rb
+++ spec/rsr_group/base_spec.rb
@@ -3,11 +3,7 @@ require 'spec_helper'
 describe RsrGroup::Base do
 
   describe ".ftp_host" do
-    it { expect(RsrGroup::Base.ftp_host).to eq('ftp.rsrgroup.com') }
-  end
-
-  describe "#connect" do
-    pending
+    it { expect(RsrGroup::Base.ftp_host).to eq('ftp.host.com') }
   end
 
 end
