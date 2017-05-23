a3c95e945479a171231f835e32d5f06e740ef16a
Justin Wilson <justin@jwils.co>
2017-05-23 10:53:19 -0400

    Support for `:end_customer_name` and `:end_customer_phone` parameters when specifying a transfer FFL

--- lib/rsr_group/order_ffl.rb
+++ lib/rsr_group/order_ffl.rb
@@ -4,7 +4,7 @@ module RsrGroup
     attr_reader :order_identifier
 
     def initialize(options = {})
-      requires!(options, :order_identifier, :licence_number, :name, :zip)
+      requires!(options, :order_identifier, :licence_number, :name, :zip, :end_customer_name, :end_customer_phone)
 
       @options = options
       @order_identifier = options[:order_identifier]
@@ -17,6 +17,8 @@ module RsrGroup
         @options[:licence_number],
         @options[:name],
         @options[:zip],
+        @options[:end_customer_name],
+        @options[:end_customer_phone]
       ].join(";")
     end
 
--- spec/rsr_group/order_ffl_spec.rb
+++ spec/rsr_group/order_ffl_spec.rb
@@ -4,19 +4,21 @@ describe RsrGroup::OrderFFL do
 
   let(:order_ffl) {
     RsrGroup::OrderFFL.new({
-      order_identifier: "AR1112",
-      licence_number: "aa-bb-01-cc",
-      name: "Balrog",
-      zip: "22122"
+      order_identifier: '100-444',
+      licence_number: 'aa-bb-01-cc',
+      name: 'Balrog',
+      zip: '22122',
+      end_customer_name: 'Gimlee',
+      end_customer_phone: '555',
     })
   }
 
-  describe "#initialize" do 
-    it { expect(order_ffl.order_identifier).to eq("AR1112") }
+  describe '#initialize' do 
+    it { expect(order_ffl.order_identifier).to eq('100-444') }
   end
 
-  describe "#to_single_line" do
-    it { expect(order_ffl.to_single_line).to eq("AR1112;11;aa-bb-01-cc;Balrog;22122") }
+  describe '#to_single_line' do
+    it { expect(order_ffl.to_single_line).to eq('100-444;11;aa-bb-01-cc;Balrog;22122;Gimlee;555') }
   end
 
 end
