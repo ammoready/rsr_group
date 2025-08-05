d796889d12a368b17822148e737e811115d27585
Hajji <hajji@ammoready.com>
2025-08-05 19:10:57 +0800

    Change back to EORD

--- lib/rsr_group/order.rb
+++ lib/rsr_group/order.rb
@@ -72,7 +72,7 @@ module RsrGroup
     end
 
     def filename
-      name = ["OORD", @merchant_number, @timestamp, @sequence_number].join("-")
+      name = ["EORD", @merchant_number, @timestamp, @sequence_number].join("-")
       [name, ".txt"].join
     end
 
